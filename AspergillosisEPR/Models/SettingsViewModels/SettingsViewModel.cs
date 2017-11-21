0cc97bc089264226a9ccd03f1f7a19ace464c3d8
Marcin Walczak <ulversson@gmail.com>
2017-11-21 09:17:48 +0000

    Add settings tabs

---
+++ AspergillosisEPR/Models/SettingsViewModels/SettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspergillosisEPR.Models.SettingsViewModels
+{
+    public class SettingsViewModel
+    {
+        public ICollection<DiagnosisCategory> DiagnosisCategories { get; set; }
+        public ICollection<DiagnosisType> DiagnosisTypes { get; set; }
+        public ICollection<Drug> Drugs { get; set; }
+
+    }
+}
