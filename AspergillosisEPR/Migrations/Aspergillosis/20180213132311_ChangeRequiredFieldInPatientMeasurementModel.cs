1410245a6c11a53b3e20305825769f1fb4535925
Marcin Walczak <ulversson@gmail.com>
2018-02-13 14:49:32 +0000

    Allow to add patient SGRQ from visits form

---
+++ AspergillosisEPR/Migrations/Aspergillosis/20180213132311_ChangeRequiredFieldInPatientMeasurementModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AspergillosisEPR.Migrations.Aspergillosis
+{
+    public partial class ChangeRequiredFieldInPatientMeasurementModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Weight",
+                table: "PatientMeasurements",
+                nullable: true,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Height",
+                table: "PatientMeasurements",
+                nullable: true,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Weight",
+                table: "PatientMeasurements",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Height",
+                table: "PatientMeasurements",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+        }
+    }
+}
