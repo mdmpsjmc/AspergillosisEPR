41f3954844bc64945530f16d5b96ed6df662543a
Marcin Walczak <ulversson@gmail.com>
2018-03-08 08:29:37 +0000

    Fix anonymous excel export

--- AspergillosisEPR/wwwroot/js/Patients.js
+++ AspergillosisEPR/wwwroot/js/Patients.js
@@ -425,7 +425,7 @@
                 var tabName = tmp.textContent.trim();
                 var tabCapitalized = tabName.charAt(0).toUpperCase() + tabName.slice(1);
                 var isChecked = index === 0 ? "checked=\"checked\"" : "";
-                var isDisabled = index === 0 ? "disabled=\"disabled\"" : "";
+                var isDisabled = (index === 0 && tabCapitalized == "Details") ? "disabled=\"disabled\"" : "";
                 var htmlOption = '<label class=\"checkbox\"><input type= \"checkbox\" ' + isDisabled + ' name= \"Show' + tabCapitalized + '\" ' + isChecked + '/><i></i> <span class=\"checkbox-label\"> ' + tabName + '</span></label>'
                 container.append(htmlOption);
             });
