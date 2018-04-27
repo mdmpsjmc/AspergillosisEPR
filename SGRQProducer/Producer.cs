86e491362355c2913cd83309b705a3ff93adaaf6
Marcin Walczak <ulversson@gmail.com>
2018-04-27 11:03:29 +0100

    Complete RabbitMQ SGRP Producer code

--- SGRQProducer/Producer.cs
+++ SGRQProducer/Producer.cs
@@ -1,7 +1,11 @@
 ﻿
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 using System;
 using System.IO;
+using System.Text;
 
 namespace SGRQProducer
 {
@@ -15,11 +19,57 @@ namespace SGRQProducer
             var builder = new ConfigurationBuilder()
                               .SetBasePath(Directory.GetCurrentDirectory())
                               .AddJsonFile("appsettings.json");
+
             configuration = builder.Build();
-            _apiClient = new SGRQApiClient(configuration);
-            configuration = builder.Build();
-            _apiClient.FetchAfterDate("2018-02-28");
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var logger = serviceProvider.GetService<ILogger<Producer>>();
             
+
+            _apiClient = new SGRQApiClient(configuration);
+
+            configuration = builder.Build();
+            logger.LogInformation("Search date is: ", args[0]);
+            var response = _apiClient.FetchAfterDate(args[0]);
+
+            if (response == null) return;
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "sgrq_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var message = response.Content; 
+                var body = Encoding.UTF8.GetBytes(message.ToCharArray());
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "sgrq_queue",
+                                     basicProperties: properties,
+                                     body: body);
+                
+                logger.LogInformation(" [x] Sent {0}", message);
+            }
         }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole())
+                .AddTransient<Producer>();            
+        }
+
+
     }
 }
--- SGRQProducer/SGRQApiClient.cs
+++ SGRQProducer/SGRQApiClient.cs
@@ -2,6 +2,7 @@
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace SGRQProducer
@@ -13,9 +14,8 @@ namespace SGRQProducer
         private string _sgrqPath;
         private string _sgrqUser;
         private string _sgrqPassword;
-        private string _filterDateParam;
-        private string _queryPath;
         private string _token;
+        CookieContainer _cookieJar = new CookieContainer();
         private static string FORM_CONTENT_TYPE = "application/x-www-form-urlencoded";
 
         public SGRQApiClient(IConfigurationRoot configuration)
@@ -25,34 +25,46 @@ namespace SGRQProducer
             _sgrqPath = configuration.GetSection("sgrqPath").Value;
             _sgrqUser = configuration.GetSection("sgrqUser").Value;
             _sgrqPassword = configuration.GetSection("sgrqPassword").Value;
-            _queryPath = configuration.GetSection("sgrqQueryPath").Value;
-            _filterDateParam = configuration.GetSection("sgrqFilterDateParam").Value;
         }
 
-        public void FetchAfterDate(string allAfterDate)
+        public IRestResponse FetchAfterDate(string allAfterDate)
         {
             var response = Login();
             if (response.IsSuccessful)
             {
                 _token = response.Content.Replace("\"", String.Empty);
                 Console.WriteLine("Obtained session token is: " + _token);
-                _queryPath = _queryPath.Replace("TOKEN_VALUE", _token);
-                FetchQuestionnaires(allAfterDate);
+                return FetchQuestionnaires(allAfterDate);
+            } else
+            {
+                var badResponse = new RestResponse();
+                badResponse.StatusCode = HttpStatusCode.Unauthorized;
+                return badResponse;
             }
         }
 
         private IRestResponse FetchQuestionnaires(string allAfterDate)
         {
-            var client = new RestClient(_baseUrl);
-            var requestPath = _sgrqPath + "/" + _queryPath + _filterDateParam + allAfterDate;
-            var request = new RestRequest(requestPath, Method.GET);
+
+            var client = new RestClient();
+            client.BaseUrl = new Uri(_baseUrl);
+            client.CookieContainer = _cookieJar;
+            var request = new RestRequest();
+            request.Resource = _sgrqPath + "/sgrq";
+            request.AddQueryParameter("transform", "1");
+            request.AddQueryParameter("csrf", _token);
+            request.AddQueryParameter("filter", "Date,gt,"+allAfterDate);
+            request.AddHeader("XSRF-TOKEN", _token);
+            request.AddHeader("Content-Type", FORM_CONTENT_TYPE);
             IRestResponse response = client.Execute(request);
-            return response;
+            if (response.IsSuccessful) return response;
+            return null;
         }
 
         private IRestResponse Login()
         {
             var client = new RestClient(_baseUrl);
+            client.CookieContainer = _cookieJar;
             var request = new RestRequest(_sgrqPath, Method.POST);
             request.AddParameter("username", _sgrqUser);
             request.AddParameter("password", _sgrqPassword);
