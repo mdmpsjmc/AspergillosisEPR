f7f05f0e963ff20e5a8021c72edc33fb41e940ce
Marcin Walczak <ulversson@gmail.com>
2018-06-01 13:20:27 +0100

    Extract large headers into separate class

--- AspergillosisEPR/Lib/Importers/Implementations/ManArtsProcessedFileImporter.cs
+++ AspergillosisEPR/Lib/Importers/Implementations/ManArtsProcessedFileImporter.cs
@@ -29,74 +29,7 @@ namespace AspergillosisEPR.Lib.Importers.Implementations
 
         public static Hashtable HeadersDictionary()
         {
-            return new Hashtable()
-            {
-                  { "Forename", "Patient.FirstName" },
-                  { "Surename", "Patient.LastName"},
-                  { "DOB", "Patient.DOB" },
-                  { "RM2", "Patient.RM2Number" },
-                  { "DEATH_TIME", "Patient.DateOfDeath" },
-                  { "DeathIndicator", "Patient.PatientStatusId"},
-                  { "Gender", "Patient.Gender" },
-                  { "NhsNumber", "Patient.NhsNumber"},
-                  { "HasCPA", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "CPAType", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "CPANotes", "Patient.DiagnosisTypeId"},
-                  { "CPAYear", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "CCPAPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasAspergilloma", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "AspergillomaYear", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "AspergillomaPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasABPA", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasAsthma", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "AsthmaNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "AsthmaYear", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "AsthmaPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasEmphysema", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasCOPD", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "COPDNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "COPDYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "COPDPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasPneumonia", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "HasFungalLungDisease", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "FungalLungDiseaseNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "FungalLungDiseaseYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "FungalLungDiseaseYearPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasChronicCough", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "ChronicCoughNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "ChronicCoughYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "ChronicCoughPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasTB", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "IsTBPrevious", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "PreviousTBNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "PreviousTBYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "PreviousTBYearPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasBronchiectasis", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "BronchiectasisNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "BronchiectasisYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "BronchiectasisPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasSarcoidosis", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "SarcoidosisNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "SarcoidosisYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "SarcoidosisPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasHypertension", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "HypertensionPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasCancer", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "CancerNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "CancerYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "CancerPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},               
-                  { "HasLungSurgery", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "LungSurgeryNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "LungSurgeryCancerYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "LungSurgeryPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasDiabetes", "PatientDiagnosis.DiagnosisTypeId" },
-                  { "DiabetesNotes", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "DiabetesYear",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "DiabetesPrimarySecondary", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasHeartDisease", "PatientDiagnosis.DiagnosisTypeId"},
-                  { "HasOsteoporosis",  "PatientDiagnosis.DiagnosisTypeId" },
-                  { "OtherDiagnosisAndNotes", "PatientDiagnosis.DiagnosisTypeId"},
-            };
+            return DiagnosisHeaders.Dictionary();
         }
 
         protected override List<string> IdentiferHeaders()
@@ -173,7 +106,7 @@ namespace AspergillosisEPR.Lib.Importers.Implementations
                 Name = diagnosisName,
                 Notes = row.GetCell(notesHeaderIndex-1, MissingCellPolicy.RETURN_BLANK_AS_NULL)?.StringCellValue,
                 Year = row.GetCell(yearsHeaderIndex-1, MissingCellPolicy.RETURN_BLANK_AS_NULL)?.StringCellValue,
-                PrimarySecondary = row.GetCell(primarySecondaryHeaderIndex-1, MissingCellPolicy.RETURN_BLANK_AS_NULL)?.StringCellValue
+                PrimarySecondary = row.GetCell(primarySecondaryHeaderIndex-1, MissingCellPolicy.RETURN_BLANK_AS_NULL)?.NumericCellValue
             };
             return manartsDiagnosis;
         }
