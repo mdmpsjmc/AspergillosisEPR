343e32fd19dff17f4cda10b38643ace4a395dc75
Marcin Walczak <ulversson@gmail.com>
2018-06-04 13:58:42 +0100

    Add patient pulmonary function test model

---
+++ AspergillosisEPR/Models/Patients/PatientPulmonaryFunctionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspergillosisEPR.Models.Patients
+{
+    public class PatientPulmonaryFunctionTest
+    {
+        public int ID { get; set; }
+        public int PatientId { get; set; }
+        public int PulmonaryFunctionTestId { get; set; }
+        public DateTime DateTaken { get; set; }
+    }
+}
