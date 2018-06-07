d54a2c94f41408386c688b2ea64098f5fd221697
Marcin Walczak <ulversson@gmail.com>
2018-06-07 11:38:25 +0100

    Add surgery models

---
+++ AspergillosisEPR/Models/Patients/PatientSurgery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspergillosisEPR.Models.Patients
+{
+    public class PatientSurgery
+    {
+        public int ID { get; set; }
+        public int SurgeryId { get; set; }
+        public int PatientId { get; set; }
+        public DateTime? SurgeryDate { get; set; }
+        public string Note { get; set; }
+    }
+}
